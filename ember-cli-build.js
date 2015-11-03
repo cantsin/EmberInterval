fbfbfb4175c37ad90891677eebbfcd4583959215
James Tranovich <jtranovich@gmail.com>
2015-11-03 11:54:47 -0800

    Added moment dependency.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,7 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
   app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
   app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+  app.import(app.bowerDirectory + '/moment/moment.js');
 
   return app.toTree();
 };
