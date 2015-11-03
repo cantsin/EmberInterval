b3a4e83adcce6524808feb6ee065c84e7c939378
James Tranovich <jtranovich@gmail.com>
2015-11-03 15:38:56 -0800

    Datetime model test.

--- tests/unit/models/datetime-test.js
+++ tests/unit/models/datetime-test.js
@@ -1,12 +1,14 @@
-import { moduleForModel, test } from 'ember-qunit';
+import { moduleForModel} from 'ember-qunit';
 
 moduleForModel('datetime', 'Unit | Model | datetime', {
   // Specify the other units that are required for this test.
   needs: []
 });
 
-test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
-});
+// we do not use ember-data here, and stub out a dummy model.
+
+// test('it exists', function(assert) {
+//   var model = this.subject();
+//   // var store = this.store();
+//   assert.ok(!!model);
+// });
