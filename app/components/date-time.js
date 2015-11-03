74aa9f619dd2e4e6fcb4392e7a97623977620901
James Tranovich <jtranovich@gmail.com>
2015-11-03 13:01:15 -0800

    The date-time component now emits formatted times

--- app/components/date-time.js
+++ app/components/date-time.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  date: function() {
+    return moment(this.data).format('YYYY-MM-DD');
+  }.property('data'),
+
+  time: function() {
+    return moment(this.data).format('HH:mm:ss');
+  }.property('data')
+
 });
