fbfe4884edc85c08ccf86b03277502ccc5016d0a
James Tranovich <jtranovich@gmail.com>
2015-11-03 16:49:00 -0800

    Interval options component tests.

--- tests/integration/components/interval-options-test.js
+++ tests/integration/components/interval-options-test.js
@@ -5,22 +5,32 @@ moduleForComponent('interval-options', 'Integration | Component | interval optio
   integration: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(2);
+test('it renders with default values', function(assert) {
+  assert.expect(6);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('from', '2015-10-20T11:00:00');
+  this.set('to', '2015-10-27T13:15:00');
 
-  this.render(hbs`{{interval-options}}`);
+  this.render(hbs`{{interval-options from=from to=to}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  let text = this.$().text();
+  assert.equal(text.indexOf('7 days 2 hours 15 minutes') >= 0, true);
+  assert.equal(text.indexOf('Option 1') >= 0, false);
+  assert.equal(text.indexOf('Option 2') >= 0, true);
+  assert.equal(text.indexOf('Option 3') >= 0, true);
+  assert.equal(text.indexOf('Option 4') >= 0, true);
+  assert.equal(text.indexOf('Option 5') >= 0, true);
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#interval-options}}
-      template block text
-    {{/interval-options}}
-  `);
+test('given nonsensical values, return zero', function(assert) {
+  assert.expect(1);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('from', 'somewhen');
+  this.set('to', 'another somewhen');
+
+  this.render(hbs`{{interval-options from=from to=to}}`);
+
+  let text = this.$().text();
+  // even though the values are nonsensical we should get zero.
+  assert.equal(text.indexOf('0 days 0 hours 0 minutes') >= 0, true);
 });
